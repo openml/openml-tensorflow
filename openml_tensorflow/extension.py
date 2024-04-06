acd709ae9d7acd1676ce8ccec23e88ed512c2b85
joaquinvanschoren <joaquin.vanschoren@gmail.com>
2024-04-06 02:44:16 +0200

    class encoding fix

--- openml_tensorflow/extension.py
+++ openml_tensorflow/extension.py
@@ -770,17 +770,25 @@ class TensorflowExtension(Extension):
         # model_copy = tensorflow.keras.models.clone_model(model, input_tensors=None, clone_function=None)
         #model_copy = pickle.loads(pickle.dumps(model))
         user_defined_measures = OrderedDict()  # type: 'OrderedDict[str, float]'
-        from sklearn import preprocessing
-        le = preprocessing.LabelEncoder()
-        X_train['encoded_labels'] = le.fit(y_train).transform(y_train)
-        X_train['encoded_labels'] = X_train['encoded_labels'].astype("string")
+
+        #from sklearn import preprocessing
+        #le = preprocessing.LabelEncoder()
+        #print("y_train",y_train)
+        #X_train['encoded_labels'] = le.fit(y_train).transform(y_train)
+        #X_train['encoded_labels'] = X_train['encoded_labels'].astype("string")
+
+        X_train['labels'] = y_train
+        #print("labels",X_train['labels'])
+        class_names = sorted(y_train.unique())
+        #print("classes", class_names)
 
         kwargs = config.kwargs if config.kwargs is not None else {}
         
         datagen = config.datagen
         train_generator = datagen.flow_from_dataframe(dataframe=X_train, directory=config.dir,
-                                            x_col=config.x_col, y_col=config.y_col,
+                                            x_col=config.x_col, y_col='labels',
                                             class_mode="categorical",
+                                            classes = class_names,
                                             target_size=config.target_size,
                                             batch_size=config.batch_size)
         
@@ -790,26 +798,31 @@ class TensorflowExtension(Extension):
             from sklearn.model_selection import train_test_split
             from tensorflow.keras.preprocessing.image import ImageDataGenerator
             
-            X_train_train, x_val = train_test_split(X_train, test_size=config.validation_split, shuffle=True, stratify=X_train[config.y_col], random_state=0)
+            # TODO: Here we're assuming that X has a label column, this won't work in general
+            X_train_train, x_val = train_test_split(X_train, test_size=config.validation_split, shuffle=True, stratify=X_train['labels'], random_state=0)
             
             datagen_train = config.datagen
-            train_generator = datagen_train.flow_from_dataframe(dataframe=X_train_train, directory=config.dir,
-                                    x_col=config.x_col, y_col=config.y_col,
-                                    class_mode="categorical",
-                                    target_size=config.target_size,
-                                    batch_size=config.batch_size)
+            train_generator = datagen_train.flow_from_dataframe(dataframe=X_train_train, 
+                                            directory=config.dir,
+                                            x_col=config.x_col, y_col='labels',
+                                            class_mode="categorical",
+                                            classes = class_names,
+                                            target_size=config.target_size,
+                                            batch_size=config.batch_size)
             
-            datagen_valid = config.datagen_valid # Don't augment the validation data!
+            datagen_valid = config.datagen_valid
             valid_generator = datagen_valid.flow_from_dataframe(dataframe=x_val,
-                                    directory=config.dir,
-                                    x_col=config.x_col, y_col=config.y_col,
-                                    class_mode="categorical",
-                                    target_size=config.target_size,
-                                    batch_size=config.batch_size)
+                                            directory=config.dir,
+                                            x_col=config.x_col, y_col='labels',
+                                            class_mode="categorical",
+                                            classes = class_names,
+                                            target_size=config.target_size,
+                                            batch_size=config.batch_size)
             
         try:
             if isinstance(task, OpenMLSupervisedTask):
-                print("starting training")
+                print(f"Testing ({len(X_train)} samples)")
+
                 
                 if config.perform_validation:
                     model_copy.fit(train_generator,
@@ -824,63 +837,54 @@ class TensorflowExtension(Extension):
                     steps_per_epoch=config.step_per_epoch,
                     epochs=config.epoch,
                     **kwargs)
-                    
                 
-                print('model_trained')
+                #print('model_trained')
 
         except AttributeError as e:
             # typically happens when training a regressor on classification task
             raise PyOpenMLError(str(e))
         
-        if isinstance(task, OpenMLClassificationTask):
-            # model_classes = tensorflow.keras.backend.argmax(X_train['encoded_labels'].astype('int'), axis=-1)
-            # I think below is th correct implementation, instead of above. Check to confirm
-            model_classes = np.sort(X_train['encoded_labels'].astype('int').unique())
-            
-        class_mapping = train_generator.class_indices  
-        classes_ordered = sorted(class_mapping, key=class_mapping.get)
+        #class_mapping = train_generator.class_indices  
+        #print("Class mapping",class_mapping)
+        #classes_ordered = sorted(class_mapping, key=class_mapping.get)
+        #print("Classes ordered",classes_ordered)
         # In supervised learning this returns the predictions for Y
-        test_generator = datagen.flow_from_dataframe(dataframe=X_test, 
+
+        #print("X test",X_test)
+        datagen_test = ImageDataGenerator()
+        test_generator = datagen_test.flow_from_dataframe(dataframe=X_test, 
                                              directory=config.dir,
-                                             class_mode="categorical",
+                                             class_mode=None,
                                              x_col=config.x_col,
-                                             y_col=config.y_col,
-                                             batch_size=1,
+                                             batch_size=32,
                                              shuffle=False,
-                                             classes=classes_ordered,
                                              target_size=config.target_size)
-        print(f"length of test data is {len(X_test)}")
+        print(f"Testing ({len(X_test)} samples)")
         if isinstance(task, OpenMLSupervisedTask):
             pred_y = model_copy.predict(test_generator)
             proba_y = pred_y
             if isinstance(task, OpenMLClassificationTask):
-                pred_y = tensorflow.keras.backend.argmax(pred_y)
-            elif isinstance(task, OpenMLRegressionTask):
-                pred_y = tensorflow.keras.backend.reshape(pred_y, (-1,))
-            pred_y = tensorflow.keras.backend.eval(pred_y)            
+                pred_y = np.argmax(pred_y, axis=-1)
+                #print("preds", pred_y)
+            #elif isinstance(task, OpenMLRegressionTask):
+            #    pred_y = tensorflow.keras.backend.reshape(pred_y, (-1,))
+            #pred_y = tensorflow.keras.backend.eval(pred_y)  
         else:
             raise ValueError(task)
 
+        # Remap the probabilities in case there was a class missing at training time
+        # By default, the classification targets are mapped to be zero-based indices
+        # to the actual classes. Therefore, the model_classes contain the correct
+        # indices to the correct probability array. Example:
+        # classes in the dataset: 0, 1, 2, 3, 4, 5
+        # classes in the training set: 0, 1, 2, 4, 5
+        # then we need to add a column full of zeros into the probabilities for class 3
+        # (because the rest of the library expects that the probabilities are ordered
+        # the same way as the classes are ordered).
         if isinstance(task, OpenMLClassificationTask):
-
-#            try:
-#                proba_y = model_copy.predict(X_test)
-#            except AttributeError:
-#                if task.class_labels is not None:
-#                    proba_y = _prediction_to_probabilities(pred_y, list(task.class_labels))
-#                else:
-#                    raise ValueError('The task has no class labels')
             if task.class_labels is not None:
                 if proba_y.shape[1] != len(task.class_labels):
-                    # Remap the probabilities in case there was a class missing at training time
-                    # By default, the classification targets are mapped to be zero-based indices
-                    # to the actual classes. Therefore, the model_classes contain the correct
-                    # indices to the correct probability array. Example:
-                    # classes in the dataset: 0, 1, 2, 3, 4, 5
-                    # classes in the training set: 0, 1, 2, 4, 5
-                    # then we need to add a column full of zeros into the probabilities for class 3
-                    # (because the rest of the library expects that the probabilities are ordered
-                    # the same way as the classes are ordered).
+                    model_classes = np.sort(X_train['labels'].astype('int').unique())
                     proba_y_new = np.zeros((proba_y.shape[0], len(task.class_labels)))
                     for idx, model_class in enumerate(model_classes):
                         proba_y_new[:, model_class] = proba_y[:, idx]
@@ -900,9 +904,14 @@ class TensorflowExtension(Extension):
         
         # Adjust prediction labels according to train_generator
         # pred_y = [int(classes_ordered[p_y]) for p_y in pred_y]
-        pred_y = le.inverse_transform(pred_y)
-        pred_y = pred_y.astype('str')
-        
+        pred_y = [class_names[i] for i in pred_y]
+
+        #pred_y = le.inverse_transform(pred_y)
+        #print("pred classes", pred_y)
+
+        #pred_y = pred_y.astype('str')
+        #print("pred inverse encoded str", pred_y)
+
         # Convert the TensorFlow model to ONNX
         onnx_model, _ = tf2onnx.convert.from_keras(model_copy, opset=13)
         onnx_ = onnx_model.SerializeToString()
